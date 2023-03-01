7a968458fdaa930ceac7dee769af853ef31f1987
Victor Stepanov <vix522430@bk.ru>
2023-03-01 17:37:42 +0300

    feat: added rev_2-task2

--- review_2/4-time /scripts/index.js
+++ review_2/4-time /scripts/index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 const NEW_YEAR_DATE = '01/01/2024';
 //DOM
 const listContainer = document.querySelector('.list');
@@ -15,25 +15,26 @@ const newYearDate = new Date(NEW_YEAR_DATE);
 
 let timer;
 buttonStart.addEventListener('click', () => {
-    timer = setInterval(() => {
-        const currentDate = new Date();
-        const difference = newYearDate - currentDate;
-        listMonth.innerHTML = Math.floor(difference / (1000 * 60 * 60 * 24 * 30) % 12)
-        listDay.innerHTML = Math.floor(difference / (1000 * 60 * 60 * 24) % 30)
-        listHours.innerHTML = Math.floor((difference / (1000 * 60 * 60)) % 24)
-        listMinuts.innerHTML = Math.floor((difference / (1000 * 60)) % 60)
-        listSeconds.innerHTML = Math.floor((difference / 1000) % 60)
-    }, 1000)
-})
+  timer = setInterval(() => {
+    const currentDate = new Date();
+    const difference = newYearDate - currentDate;
+    listMonth.innerHTML = Math.floor(
+      (difference / (1000 * 60 * 60 * 24 * 30)) % 12
+    );
+    listDay.innerHTML = Math.floor((difference / (1000 * 60 * 60 * 24)) % 30);
+    listHours.innerHTML = Math.floor((difference / (1000 * 60 * 60)) % 24);
+    listMinuts.innerHTML = Math.floor((difference / (1000 * 60)) % 60);
+    listSeconds.innerHTML = Math.floor((difference / 1000) % 60);
+  }, 1000);
+});
 
-buttonReset.addEventListener('click', (evt) => {
-    if (evt) {
-        clearInterval(timer)
-        listMonth.innerHTML = 0;
-        listDay.innerHTML = 0
-        listHours.innerHTML = 0
-        listMinuts.innerHTML = 0
-        listSeconds.innerHTML = 0
-
-    }
-})
+buttonReset.addEventListener('click', evt => {
+  if (evt) {
+    clearInterval(timer);
+    listMonth.innerHTML = 0;
+    listDay.innerHTML = 0;
+    listHours.innerHTML = 0;
+    listMinuts.innerHTML = 0;
+    listSeconds.innerHTML = 0;
+  }
+});
