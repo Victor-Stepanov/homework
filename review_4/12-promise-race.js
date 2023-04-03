ad941e753fc8af49ad13a8e8fa5e720f18063e0a
Victor Stepanov <vix522430@bk.ru>
2023-04-03 15:01:59 +0300

    feat: added test data

--- review_4/12-promise-race.js
+++ review_4/12-promise-race.js
@@ -1,3 +1,12 @@
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 500, 'one');
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'two');
+});
+
+
 function implementRace(promises) {
   return new Promise((resolve, reject) => {
     promises.forEach(promise => {
